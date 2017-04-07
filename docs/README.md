27c72aa8699049b41682e2347549fc8348542846
Clef-0 <spaceyavin@gmail.com>
2017-04-07 11:47:50 +0100

    jekyll update

--- docs/README.md
+++ docs/README.md
@@ -1,32 +1,16 @@
-Jekyll Photo Grid Gallery
-=========
+[![Build Status](https://travis-ci.org/madebyconnor/physalis.svg?branch=master)](https://travis-ci.org/madebyconnor/physalis) [![Codacy Badge](https://api.codacy.com/project/badge/Grade/5869a9200876413f8d8e51b37e0dcfa5)](https://www.codacy.com/app/connor_baer/physalis?utm_source=github.com&amp;utm_medium=referral&amp;utm_content=madebyconnor/physalis&amp;utm_campaign=Badge_Grade)
 
-Jekyll theme based on [Blueprint: Google Grid Gallery](http://tympanus.net/codrops/?p=18699)
+# The README Checklist
 
-## How to use
-1. Place a large image in `/img/large/`
-2. Place a thumbnail version of the same image in `/img/thumb/`. _Remember to use the same file name_
-3. Construction a post to display the image. Use the follow as an example:
-```txt
----
-layout: default
-date:   2014-04-06 23:26:12
-photo: 1.png
-caption_header: Sample header
-caption: sample caption
----
-```
-4. And finally, serve Jekyll source as you would normally.
+A handy checklist for an efficient README:
 
-## [Demo](https://iamnii.github.io/GridGallery)
-
-View this jekyll theme in action [here](https://iamnii.github.io/GridGallery)
-
-## Screenshot
-![screenshot](https://raw.github.com/iamnii/GridGallery/master/jggg.png)
-
-Localhost deployment
-=========
-After [downloading](https://github.com/iamnii/GridGallery/archive/master.zip), use the normal process of serving a jekyll site. Navigate to the directory in terminal and type `jekyll build` followed by `jekyll serve`
-
-For more details, read [documentation](http://jekyllrb.com/)
+ - [ ] One-liner explaining the purpose of the module
+ - [ ] Necessary background context & links
+ - [ ] Potentially unfamiliar terms link to informative sources
+ - [ ] Clear, runnable example of usage
+ - [ ] Installation instructions
+ - [ ] Extensive API documentation
+ - [ ] Performs cognitive funneling
+ - [ ] Caveats and limitations mentioned up-front
+ - [ ] Doesn't rely on images to relay critical information
+ - [ ] License
---
+++ docs/_js/custom.js
@@ -0,0 +1,111 @@
+/* jshint -W117 */
+/* jshint -W098 */
+/* jshint -W070 */
+
+document.addEventListener('turbolinks:load', function () {
+
+  // LISTJS //
+
+  // Define the list options.
+  var searchOptions = {
+    valueNames: [
+      'title',
+      'category',
+      'tags',
+      'duration',
+      { name: 'ingredients', attr: 'data-ingredients' },
+    ],
+    plugins: [
+      ListFuzzySearch(),
+    ],
+  };
+
+  // Define the list object.
+  recipeList = new List('js-list', searchOptions);
+
+  var checkCategory = document.getElementsByClassName('js-category');
+  var checkDuration = document.getElementsByClassName('js-duration');
+  var checkMeat = document.getElementsByClassName('js-meat');
+  var checkOrigin = document.getElementsByClassName('js-origin');
+
+  var checkBoxes = document.getElementsByClassName('searchbar-checkbox');
+
+  // Filter the list with the selected filters.
+  function filterList() {
+    var checkedCategory = [];
+    var checkedDuration = [];
+    var checkedMeat = [];
+    var checkedOrigin = [];
+
+    // Put the checked categories into an array.
+    for (var m = 0; m < checkCategory.length; ++m) {
+      if (checkCategory[m].checked) {
+        var valueCategory = checkCategory[m].value;
+        checkedCategory.push(valueCategory);
+      }
+    }
+
+    // Put the checked durations into an array.
+    for (var n = 0; n < checkDuration.length; ++n) {
+      if (checkDuration[n].checked) {
+        var valueDuration = checkDuration[n].value;
+        checkedDuration.push(valueDuration);
+      }
+    }
+
+    // Put the checked tags into an array.
+    for (var o = 0; o < checkMeat.length; ++o) {
+      if (checkMeat[o].checked) {
+        var valueMeat = checkMeat[o].value;
+        checkedMeat.push(valueMeat);
+      }
+    }
+
+    // Put the checked tags into an array.
+    for (var p = 0; p < checkOrigin.length; ++p) {
+      if (checkOrigin[p].checked) {
+        var valueOrigin = checkOrigin[p].value;
+        checkedOrigin.push(valueOrigin);
+      }
+    }
+
+    var checkedLength = checkedCategory.length + checkedDuration.length + checkedMeat.length + checkedOrigin.length;
+
+    if (checkedLength > 0) {
+
+      // Check if the item matches the filters.
+      recipeList.filter(function (item) {
+
+        // Check if in category.
+        var category = checkedCategory.length === 0 || checkedCategory.indexOf(item.values().category) > -1;
+
+        // Check if duration is shorter.
+        var duration = checkedDuration.length === 0 || checkedDuration >= item.values().duration;
+
+        // Check if item has meat.
+        var meat = checkedMeat.length === 0 || checkedMeat.filter(function(n) { return item.values().tags.split(', ').indexOf(n) !== -1; }).length > 0;
+
+        // Check where item originates.
+        var origin = checkedOrigin.length === 0 || checkedOrigin.filter(function(n) { return item.values().tags.split(', ').indexOf(n) !== -1; }).length > 0;
+
+        // Show the item if it matches the filters.
+        if (category && duration && meat && origin) {
+          return true;
+        }
+
+        return false;
+      });
+    } else {
+
+      // No filters, so clear all filters.
+      recipeList.filter();
+    }
+  }
+
+  // Filter list when checkbox is clicked.
+  if (checkBoxes) {
+    for (var k = 0; k < checkBoxes.length; ++k) {
+      checkBoxes[k].addEventListener('change', filterList);
+    }
+  }
+});
